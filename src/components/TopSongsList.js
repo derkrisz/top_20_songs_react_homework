c59c1062b6e310f7ca45179a3ec9874204ce9c42
derkrisz <krisztian.der@gmail.com>
2018-02-21 06:46:49 +0000

    fix position error

--- src/components/TopSongsList.js
+++ src/components/TopSongsList.js
@@ -5,7 +5,7 @@ const TopSongsList = (props) => {
 
   let songsList = props.topSongs.map((song, index) => {
     return (
-      <TopSongsDetail key={index} img={song['im:image']} position={index} title={song['im:name'].label} artist={song['im:artist'].label} />
+      <TopSongsDetail key={index} img={song['im:image']} position={index+1} title={song['im:name'].label} artist={song['im:artist'].label} />
     )
   })
 
